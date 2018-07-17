9aa6a3f00426d953a2293cd50babce378e673f86
johannahmae3 <johannahmae19@gmail.com>
2018-07-17 16:51:14 +0800

    commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function clear() {
+    document.getElementById("total").value
+  }
+  function add(){
+
+  }
+  function sub(){
+    
+  }
+  function multi(){
+    
+  }
+  function divide(){
+    
+  }
+  function seven(){
+    document.getElementById('total').value += '7';
+  }
